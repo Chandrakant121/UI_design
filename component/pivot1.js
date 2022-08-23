1bb41826f191c92ce80fa23fb1f391bb5075d70c
Chandrakant121 <csjoshi989@gmail.com>
2022-08-23 22:26:06 +0530

    first commit

---
+++ component/pivot1.js
@@ -0,0 +1,138 @@
+function pivot1() {
+    return `
+<div class="pivotbox1">
+<div class="pivotimg1">
+    <img src="./Images/edugrp.png" alt="">
+    <h2>What's in it for Teachers</h2>
+    <ul>
+        <li>
+            Familiar and easy, always up-to-date tools to reach
+        </li>
+        <li>
+            Access to latest version of Windows 10
+        </li>
+        <li>
+            Anytime anywhere access to Office 365 across multiple devices
+        </li>
+    </ul>
+</div>
+
+<div class="pivotimg2">
+    <img src="./Images/edu.png" alt="">
+</div>
+
+</div>
+<div class="pivotbox2">
+<div class="pivotimg2">
+    <img src="./Images//edugirls.png" alt="">
+</div>
+
+<div class="pivotimg3">
+    <img src="./Images/graduate.png" alt="">
+    <h2>What's in it for Students</h2>
+    <ul>
+        <li>
+            Be productive anywhere with Office 365, accessible anytime across multiple devices
+        </li>
+        <li>
+            Connect and collaborate with other students and friends, track reviews and inputs through
+            updates
+        </li>
+    </ul>
+</div>
+</div>`
+}
+
+function pivot2() {
+    return `
+    <div class="pivotbox2">
+<div class="pivotimg2">
+    <img src="./Images//edugirls.png" alt="">
+</div>
+
+<div class="pivotimg3">
+    <img src="./Images/graduate.png" alt="">
+    <h2>What's in it for Students</h2>
+    <ul>
+        <li>
+            Be productive anywhere with Office 365, accessible anytime across multiple devices
+        </li>
+        <li>
+            Connect and collaborate with other students and friends, track reviews and inputs through
+            updates
+        </li>
+    </ul>
+</div>
+</div>
+<div class="pivotbox1">
+<div class="pivotimg1">
+    <img src="./Images/edugrp.png" alt="">
+    <h2>What's in it for Teachers</h2>
+    <ul>
+        <li>
+            Familiar and easy, always up-to-date tools to reach
+        </li>
+        <li>
+            Access to latest version of Windows 10
+        </li>
+        <li>
+            Anytime anywhere access to Office 365 across multiple devices
+        </li>
+    </ul>
+</div>
+
+<div class="pivotimg2">
+    <img src="./Images/edu.png" alt="">
+</div>
+
+</div>
+`
+}
+
+
+function pivot3() {
+    return `
+    <div class="pivotbox2">
+        <div class="pivotimg3">
+            <img src="./Images/graduate.png" alt="">
+            <h2>What's in it for Students</h2>
+            <ul>
+                <li>
+                    Be productive anywhere with Office 365, accessible anytime across multiple devices
+                </li>
+                <li>
+                    Connect and collaborate with other students and friends, track reviews and inputs through
+                    updates
+                </li>
+            </ul>
+        </div>
+        <div class="pivotimg2">
+            <img src="./Images//edugirls.png" alt="">
+        </div>
+    </div>
+    <div class="pivotbox1">
+    <div class="pivotimg2">
+        <img src="./Images/edu.png" alt="">
+    </div>
+    <div class="pivotimg1">
+        <img src="./Images/edugrp.png" alt="">
+        <h2>What's in it for Teachers</h2>
+        <ul>
+            <li>
+                Familiar and easy, always up-to-date tools to reach
+            </li>
+            <li>
+                Access to latest version of Windows 10
+            </li>
+            <li>
+                Anytime anywhere access to Office 365 across multiple devices
+            </li>
+        </ul>
+    </div>
+</div>
+    
+`
+}
+
+
+export { pivot1, pivot2, pivot3 }
